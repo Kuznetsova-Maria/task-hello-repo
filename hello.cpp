6c8158c9ee4aff218c923cd35e09a3a908afed96
Kuznetsova Maria <mariakuznetsova64034@gmail.com>
2024-04-07 21:28:29 +0300

    Add print of all Fibonacci numbers to n, starting from 0,1...

--- hello.cpp
+++ hello.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 
-int fib(int n) {
-    if (n <= 0)
-        return 0;
-    if (n == 2)
-        return 1;
-    return fib(n - 1) + fib(n - 2);
+void fib(int n){
+    int a1 = 0, a2 = 1, a3 ;
+    if (n >= 1) std::cout << a1 << "\n";
+    if (n >= 2) std::cout << a2 << "\n";
+    for (int i = 3; i <= n; i++){
+        int a3 = a1 + a2;
+        std::cout << a3 << "\n";
+        a1 = a2;
+        a2 = a3;
+    }
 }
 
 int main() {
     std::cout << "Hello, World!\n";
     int n;
     std::cin >> n;
-    std::cout << fib(n);
+    fib(n);
 }
